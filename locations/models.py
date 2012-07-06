ed47da1e2d48d4c33e240de5863746e557688329
mercutio22 <mnemonico@posthocergopropterhoc.net>
2012-07-06 14:13:02 -0300

    new translation strings for the models

--- locations/models.py
+++ locations/models.py
@@ -10,6 +10,8 @@ class Country(models.Model):
 
     class Meta:
         ordering = ['name']
+        verbose_name = _('Country')
+        verbose_name_plural = _('Countries')
 
 class Region(models.Model):
     name = models.CharField(_(u'Region'), max_length=255, unique=True)
@@ -20,7 +22,9 @@ class Region(models.Model):
 
     class Meta:
         ordering = ['name']
-
+        verbose_name = _('Region')
+        verbose_name_plural = _('Regions')
+        
 class State(models.Model):
     name = models.CharField(_(u'State'), max_length=255, unique=True, db_index=True)
     code = models.CharField(_(u'Code'), max_length=2, unique=True)
@@ -31,6 +35,8 @@ class State(models.Model):
 
     class Meta:
         ordering = ['name']
+        verbose_name = _('State')
+        verbose_name_plural = _('States')
 
 class MesoRegion(models.Model):
     name = models.CharField(_(u'Mesoregion'), max_length=255, unique=True)
@@ -41,6 +47,8 @@ class MesoRegion(models.Model):
 
     class Meta:
         ordering = ['name']
+        verbose_name = _('mesoregion')
+        verbose_name_plural = _('mesoregions')
 
 class MicroRegion(models.Model):
     name = models.CharField(_(u'Microregion'), max_length=255)
@@ -52,7 +60,9 @@ class MicroRegion(models.Model):
     class Meta:
         ordering = ['name']
         unique_together = ('name', 'mesoregion')
-
+        verbose_name = _('microregion')
+        verbose_name_plural = _('microregions')
+        
 class Municipality(models.Model):
     name = models.CharField(_(u'Municipality'), max_length=255, db_index=True)
     microregion = models.ForeignKey(MicroRegion)
@@ -63,6 +73,8 @@ class Municipality(models.Model):
     class Meta:
         ordering = ['name']
         unique_together = ('name', 'microregion')
+        verbose_name = _('municipality')
+        verbose_name_plural = _('municipalities')
 
 class District(models.Model):
     name = models.CharField(_(u'District'), max_length=255)
@@ -74,7 +86,9 @@ class District(models.Model):
     class Meta:
         ordering = ['name']
         unique_together = ('name', 'municipality')
-
+        verbose_name = _('district')
+        verbose_name_plural = _('districts')
+        
 class Neighborhood(models.Model):
     name = models.CharField(_(u'Neighborhood'), max_length=255)
     district = models.ForeignKey(District)
@@ -85,13 +99,19 @@ class Neighborhood(models.Model):
     class Meta:
         ordering = ['name']
         unique_together = ('name', 'district')
-
+        verbose_name = _('neighborhood')
+        verbose_name_plural = _('neighborhoods')
+        
 class Coordinate(models.Model):
     latitude = models.FloatField(_('latitude'), blank=True, null=True)
     longitude = models.FloatField(_('longitude'), blank=True, null=True)
     postal_code = models.CharField(_('postal code'), max_length=10, blank=True, null=True)
     street_name = models.CharField(_('street name'), max_length=255)
     street_number = models.CharField(_('street number'), max_length=24)
+    
+    class Meta:
+        verbose_name = _('coordinate')
+        verbose_name_plural = _('coordinates')
 
 class Place(models.Model):
     municipality = models.ForeignKey(Municipality)
@@ -127,3 +147,7 @@ class Place(models.Model):
             self.coordinate.street_name,
             self.coordinate.street_number,
             self.municipality)
+
+    class Meta:
+        verbose_name = _('Place')
+        verbose_name_plural = _('Places')
